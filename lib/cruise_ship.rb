0341166c1ee89da39bad46f9a07f183be0d69d74
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-20 17:46:39 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,14 @@
 # suite_e: "Crumpet the Elf"
 # }
 
+# returns the name of the passenger who stays in suite a and whose name begins with the letter "A"
+
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name| 
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+  winner
 end
